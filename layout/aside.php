f131a83f88a39ce82f95fde6b25e67470566c4af
DrewWeth <dgwetherington@gmail.com>
2014-05-15 04:50:03 -0500

    hs sidebar

--- layout/aside.php
+++ layout/aside.php
@@ -8,6 +8,7 @@
 		if (user_logged_in() && is_admin($user_data)) include 'layout/widgets/Wadmin.php'; 
 		include 'layout/widgets/serverinfo.php';
 		include 'layout/widgets/topplayers.php';
+		include 'layout/widgets/highscores.php';
 		//if ($config['TFSVersion'] === 'TFS_03') include 'layout/widgets/houses.php';
 		// Remove // to enable twitter, edit twitter stuff in /widgets/twitter.php
 		//include 'layout/widgets/twitter.php';
