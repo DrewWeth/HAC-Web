087b3ba50f2c0af4823aadb71016c0b191de1f77
DrewWeth <dgwetherington@gmail.com>
2014-05-15 01:21:57 -0500

    side bars

--- layout/widgets/serverinfo.php
+++ layout/widgets/serverinfo.php
@@ -1,44 +1,46 @@
 <div class="sidebar">
-	<h2>Server Information</h2>
-	<div class="inner">
-		<table class="table table-striped">
-			<tr>
-				<td>
-					<?php
-					$status = true;
-					if ($config['status']['status_check']) {
-						@$sock = fsockopen ($config['status']['status_ip'], $config['status']['status_port'], $errno, $errstr, 1);
-						if(!$sock) {
-							echo "<div class=\"label label-danger\">Server Offline</div>";
-							$status = false;
+	<div class="container">
+		<h2>Server Information</h2>
+		<div class="inner">
+			<table class="table table-striped">
+				<tr>
+					<td>
+						<?php
+						$status = true;
+						if ($config['status']['status_check']) {
+							@$sock = fsockopen ($config['status']['status_ip'], $config['status']['status_port'], $errno, $errstr, 1);
+							if(!$sock) {
+								echo "<div class=\"label label-danger\">Server Offline</div>";
+								$status = false;
+							}
+							else {
+								$info = chr(6).chr(0).chr(255).chr(255).'info';
+								fwrite($sock, $info);
+								$data='';
+								while (!feof($sock))$data .= fgets($sock, 1024);
+								fclose($sock);
+								echo "<div class=\"label label-success\">Server Online!</div>";
+							}
 						}
-						else {
-							$info = chr(6).chr(0).chr(255).chr(255).'info';
-							fwrite($sock, $info);
-							$data='';
-							while (!feof($sock))$data .= fgets($sock, 1024);
-							fclose($sock);
-							echo "<div class=\"label label-success\">Server Online!</div>";
+						?>
+					</td>
+				</tr>
+				<tr>
+					<td>
+						<?php if ($status) {
+						?>
+							<a href="onlinelist.php"><?php echo user_count_online();?> player online</a>
+						<?php
 						}
-					}
-					?>
-				</td>
-			</tr>
-			<tr>
-				<td>
-					<?php if ($status) {
-					?>
-						<a href="onlinelist.php"><?php echo user_count_online();?> player online</a>
-					<?php
-					}
-					?>
-				</td>
-			</tr>
-			<tr>
-				<td>
-					<a href="register.php"><?php echo user_count_accounts();?> registered accounts</a>
-				</td>
-			</tr>
-		</table>
+						?>
+					</td>
+				</tr>
+				<tr>
+					<td>
+						<a href="register.php"><?php echo user_count_accounts();?> registered accounts</a>
+					</td>
+				</tr>
+			</table>
+		</div>
 	</div>
 </div>
--- layout/widgets/topplayers.php
+++ layout/widgets/topplayers.php
@@ -1,24 +1,24 @@
 <div class="sidebar">
-	<h2>Top 5 players</h2>
-	<?php
+	<div class="container">
+		<h2>Top 5 players</h2>
+		<?php
+		$cache = new Cache('engine/cache/topPlayer');
+		if ($cache->hasExpired()) {
+			$players = mysql_select_multi("SELECT `name`, `level`, `experience` FROM `players` ORDER BY `experience` DESC LIMIT 5;");
+			
+			$cache->setContent($players);
+			$cache->save();
+		} else {
+			$players = $cache->load();
+		}
 
-	$cache = new Cache('engine/cache/topPlayer');
-	if ($cache->hasExpired()) {
-		$players = mysql_select_multi("SELECT `name`, `level`, `experience` FROM `players` ORDER BY `experience` DESC LIMIT 5;");
-		
-		$cache->setContent($players);
-		$cache->save();
-	} else {
-		$players = $cache->load();
-	}
-
-	$count = 1;
-	echo "<table>";
-	foreach($players as $player) {
-		echo "<tr><td>$count</td><td><a href='characterprofile.php?name=". $player['name']. "'>". $player['name']. "</a> (". $player['level'] .")</td></tr>";
-		$count++;
-	}
-	echo "</table>";
-	?>
-	<br>
+		$count = 1;
+		echo "<table class=\"table table-striped\">";
+		foreach($players as $player) {
+			echo "<tr><td>$count</td><td><a href='characterprofile.php?name=". $player['name']. "'>". $player['name']. "</a> (". $player['level'] .")</td></tr>";
+			$count++;
+		}
+		echo "</table>";
+		?>
+	</div>
 </div>
