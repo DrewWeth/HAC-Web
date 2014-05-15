8bd17ae51da7d44e29f81cabdd25399a6452798c
DrewWeth <dgwetherington@gmail.com>
2014-05-15 04:14:16 -0500

    jquery on top

--- layout/head.php
+++ layout/head.php
@@ -3,11 +3,12 @@
   <meta name="description" content="website description" />
   <meta name="keywords" content="website keywords, website keywords" />
   <meta http-equiv="content-type" content="text/html; charset=UTF-8" />
-  <link rel="stylesheet" type="text/css" href="layout/css/bootstrap.css" />
-  <link rel="stylesheet" type="text/css" href="layout/css/style.css" />
+    <script type='text/javascript' src='layout/js/jquery-2.1.1.min.js'></script>
 
   <script type='text/javascript' src="layout/js/bootstrap.min.js"></script>
-  <script type='text/javascript' src='layout/js/jquery-2.1.1.min.js'></script>
+
+  <link rel="stylesheet" type="text/css" href="layout/css/bootstrap.css" />
+  <link rel="stylesheet" type="text/css" href="layout/css/style.css" />
 
 
 
