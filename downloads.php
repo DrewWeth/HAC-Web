c74ca1a920ec44ce62b3d49b8a353a04c1bd2151
DrewWeth <dgwetherington@gmail.com>
2014-05-17 00:52:23 -0500

    hr

--- downloads.php
+++ downloads.php
@@ -1,31 +1,26 @@
 <?php require_once 'engine/init.php'; include 'layout/overall/header.php'; ?>
 	<center><h2>Downloads</h2></center>
-	<h3>Necessary Downloads</h3>
-	<p>In order to play, you need a Tibia client.</p>
-	<ul>
-	<li>Download otland IP changer <a href="http://static.otland.net/ipchanger.exe">HERE</a>.</li>
-	<li>Download Tibia client <?php echo ($config['client'] / 100); ?> <a href="<?php echo $config['client_download']; ?>">HERE</a>.</li>
-	</ul>
-	<br>
-	<h3>How to connect and play</h3>
-	<ol>
-		<li>
-			<a href="http://remeresmapeditor.com/rmedl.php?file=tibia<?php echo ($config['client']); ?>.exe">Download</a>, install and start the tibia client if you havent already.
-		</li>
-		<li>
-			<a href="http://static.otland.net/ipchanger.exe">Download</a> and run the IP changer.
-		</li>
-		<li>
-			In the IP changer, write this in the IP field: <?php echo $_SERVER['SERVER_NAME']; ?>
-		</li>
-		<li>
-			In the IP changer, write this in the Port field: <?php echo $config['port']; ?>
-		</li>
-		<li>
-			Now you can successfully login on the tibia client and play. <br>
-			If you do not have an account to login with, you need to register an account <a href="register.php">HERE</a>.
-		</li>
-	</ol>
+	<h3>Connection Information</h3>
+	<tbody>
+		<table class="table table-bordered">
+			<tr>
+				<td style="width:50%"><center><b>Custom Client</b></center></td><td><center><b>Original Client</b></center></td>
+			</tr>
+			<tr>
+				<td><center><a href="https://dl.dropboxusercontent.com/u/7902004/hac_client.zip"><img class="img-responsive" src="https://farm3.staticflickr.com/2912/14016545650_9866bd310b_o.gif"></a></center></td>
+				<td>
+					<label>Step One</label>
+					<a href="http://remeresmapeditor.com/rmedl.php?file=tibia<?php echo ($config['client']); ?>.exe">Download</a>, install, and start the tibia client if you havent already.
+					<label>Step Two</label>
+					<a href="http://static.otland.net/ipchanger.exe">Download</a> and run the IP changer with the IP as: <?php echo $_SERVER['SERVER_NAME']; ?>
+				</td>
+			</tr>		
+		</table>
+	</tbody>
+	<hr>
+	<li>Client version: <?php echo ($config['client'])/100.0; ?></li>
+	<li>IP address: <?php echo $_SERVER['SERVER_NAME']; ?></li>
+	<hr>
 
 <?php 
 include 'layout/overall/footer.php'; ?>
--- serverinfo.php
+++ serverinfo.php
@@ -1,27 +1,9 @@
 <?php require_once 'engine/init.php'; include 'layout/overall/header.php'; ?>
 	<center><h1>Server Information</h1></center>
 	<ul>
-	<h3>Connection Information</h3>
-	<tbody>
-		<table class="table table-bordered">
-			<tr>
-				<td style="width:50%"><center><b>Custom Client</b></center></td><td><center><b>Original Client</b></center></td>
-			</tr>
-			<tr>
-				<td><center><a href="https://dl.dropboxusercontent.com/u/7902004/hac_client.zip"><img class="img-responsive" src="https://farm3.staticflickr.com/2912/14016545650_9866bd310b_o.gif"></a></center></td>
-				<td>
-					<a href="http://remeresmapeditor.com/rmedl.php?file=tibia<?php echo ($config['client']); ?>.exe">Download</a>, install, and start the tibia client if you havent already.
-				</td>
-			</tr>
-			<tr>
-				<td></td><td><a href="http://static.otland.net/ipchanger.exe">Download</a> and run the IP changer with the IP as: <?php echo $_SERVER['SERVER_NAME']; ?>
-</td>
-			</tr>
-		</table>
-	</tbody>
-	<hr>
-	<li>Client version: <?php echo ($config['client'])/100.0; ?></li>
-	<li>IP address: <?php echo $_SERVER['SERVER_NAME']; ?></li>
+		<li>Client version: <?php echo ($config['client'])/100.0; ?></li>
+		<li>IP address: <?php echo $_SERVER['SERVER_NAME']; ?></li>
+	</ul>
 	<hr>
 	<h3>Experience Stages</h3>
 	<tbody>
