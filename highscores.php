c73896287c5d4198c9e4050cf7726681660256d4
DrewWeth <dgwetherington@gmail.com>
2014-05-16 22:46:07 -0500

    a

--- highscores.php
+++ highscores.php
@@ -4,39 +4,41 @@ if ($config['log_ip']) {
 }
 ?>
 <div class="well">
-	<div class="col-sm-3">
-		<tbody>
-			<table class="table table-condensed">
-				<tr>
-					<td class="center">
-						<form action="highscores.php" class="form-control" method="post">
-							<select name="selected" class="form-control">
-							<option value="7">Experience</option>
-							<option value="5">Shielding</option>
-							<option value="3">Axe</option>
-							<option value="2">Sword</option>
-							<option value="1">Club</option>
-							<option value="4">Distance</option>
-							<option value="0">Fist</option>
-							<option value="6">Fish</option>
-							<option value="8">Magic</option>
-							</select> 
-							<?php
-								/* Form file */
-								Token::create();
-							?>
+	<div class="row">
+		<div class="span3">
+			<tbody>
+				<table class="table table-condensed">
+					<tr>
+						<td class="center">
+							<form action="highscores.php" class="form-control" method="post">
+								<select name="selected" class="form-control">
+								<option value="7">Experience</option>
+								<option value="5">Shielding</option>
+								<option value="3">Axe</option>
+								<option value="2">Sword</option>
+								<option value="1">Club</option>
+								<option value="4">Distance</option>
+								<option value="0">Fist</option>
+								<option value="6">Fish</option>
+								<option value="8">Magic</option>
+								</select> 
+								<?php
+									/* Form file */
+									Token::create();
+								?>
+							</td>
+						</tr>
+						<tr>
+							<td>
+								<input type="submit" class="btn btn-primary" value="Fetch scoreboard">
+							</form>
 						</td>
 					</tr>
-					<tr>
-						<td>
-							<input type="submit" class="btn btn-primary" value="Fetch scoreboard">
-						</form>
-					</td>
-				</tr>
-			</table>
-		</tbody>
+				</table>
+			</tbody>
+		</div>
 	</div>
-	<div class="col-sm-9">
+	<div class="span-9">
 		<?php
 		if (empty($_POST) === false) {
 			
@@ -121,6 +123,7 @@ if ($config['log_ip']) {
 						</table>
 					</tbody>
 					</div>
+				</div>
 				</div>
 					<?php
 				} else {
