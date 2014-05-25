5a69586fcde9021069345b307973fd56a3a9c639
DrewWeth <dgwetherington@gmail.com>
2014-05-24 21:51:35 -0500

    link color

--- layout/widgets/login.php
+++ layout/widgets/login.php
@@ -5,12 +5,9 @@
 			<table class="table table-condensed table-striped">
 				<tr>
 					<form action="login.php" method="post">
-					<ul id="login">
 					<td>
 						Userame
 					</td>
-				</tr>
-				<tr>
 					<td>
 						<input class="form-control input-sm" type="text" name="username">
 					</td>
@@ -19,23 +16,18 @@
 					<td>
 						Password
 					</td>
-				</tr>
 					<td>
 						<input class="form-control input-sm" type="password" name="password">
 					</td>
 				</tr>
-				<tr>
-					<td colspan="2">
+			</table>
+		</tbody>
 						<center><input type="submit" class="btn btn-primary btn-sm" value="Log in"></center>
-					</td>
 					<?php
 						/* Form file */
 						Token::create();
 					?>
-				</tr>
 				</form>
-			</table>
-		</tbody>
 		<center><a href="register.php">Create Account</a></center>
 		<center>Lost <a href="recovery.php?mode=username">username</a> or <a href="recovery.php?mode=password">password</a>?</center>		
 	</div>
