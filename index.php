3e7a16518a6d644268c7a4d2bee0c78edf94c3af
DrewWeth <dgwetherington@gmail.com>
2014-06-07 10:48:01 -0500

    wells

--- index.php
+++ index.php
@@ -41,10 +41,13 @@
 			?>
 					<h2>
 						<?php echo TransformToBBCode($n['title'])."</h2><div class=\"news-post-date\">".date($config['date'], $n['date'])."</div>"; ?>
-						<hr>
+		
 					<p><?php echo TransformToBBCode(nl2br($n['text'])); ?></p>
+			<?php {
+				if ($i == 1)
+					echo '</div>';
+			} ?>
 			</div>
-			
 			<?php
 			$i = $i + 1;
 			}
